c649b4c16d33637841cd860ff8f0ca955124e6a9
Tommy093 <tomek.wloszkiewicz@gmail.com>
2017-11-03 18:51:54 +0100

    czyszczenie maina

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Lukasz jest najlepszym programista");
-        System.out.println("Wojtek tez jest dobrym programista");
-        System.out.println("Darek");
-        System.out.println("Kupa");
         }
 }

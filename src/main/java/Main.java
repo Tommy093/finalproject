9f80eb10678fddb0b87baf6197730ef8e6ffb12e
wojciechswiderski1 <bajbol@gmail.com>
2017-11-03 18:34:50 +0100

    3 linijki sout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Lukasz jest najlepszym programista");
+        System.out.println("Wojtek tez jest dobrym programista");
+        System.out.println("Darek");
         }
 }

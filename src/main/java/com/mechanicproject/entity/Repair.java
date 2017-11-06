d121a6d364a10c1cde42539564826cd97e2dd219
wojciechswiderski1 <bajbol@gmail.com>
2017-11-06 18:00:13 +0100

    -adn. @entity w kl. Repair -pole prywatne CarRepository w kl. DefaultCarRepository -w application.repository zamiast spring.jpa.hibernate.ddl-auto=validate ustawiono =create

--- src/main/java/com/mechanicproject/entity/Repair.java
+++ src/main/java/com/mechanicproject/entity/Repair.java
@@ -1,11 +1,13 @@
 package com.mechanicproject.entity;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
+@Entity
 public class Repair {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/com/mechanicproject/service/CarService.java
@@ -0,0 +1,8 @@
+package com.mechanicproject.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CarService {
+
+}

c5473dd7f95c775c4c8f7b5a539e2c91dc740c43
Tommy093 <tomek.wloszkiewicz@gmail.com>
2017-11-07 19:49:42 +0100

    encja Role, nowy kastomer

--- src/main/java/com/mechanicproject/entity/Customer.java
+++ src/main/java/com/mechanicproject/entity/Customer.java
@@ -2,89 +2,119 @@ package com.mechanicproject.entity;
 
 //import org.springframework.security.core.GrantedAuthority;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 @Entity
-public class Customer {
+public class Customer implements UserDetails{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+    @NotNull
    // @Size(min = 2)
-    String name;
+    private String name;
 
     @NotNull
-    String surname;
+    private String surname;
 
     @NotNull
-    String phoneNumber;
-
-    @NotNull
-    Integer customer_id;
+    private String phoneNumber;
 
     @OneToMany
     private List<Car> carList;
 
+    @ManyToMany (fetch = FetchType.EAGER)
+    private Set<Role> roleSet;
 
-
-    public Customer(String name, String surname, String phoneNumber, Integer customer_id) {
+    public Customer(String name, String surname, String phoneNumber) {
         this.name = name;
         this.surname = surname;
         this.phoneNumber = phoneNumber;
-        this.customer_id = customer_id;
+    }
+
+    public Customer() {
+
+    }
+
+    public Customer(String username, String password, String name, String surname, String phoneNumber) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
     }
 
     public Integer getId() {
         return id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoleSet();
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getSurname() {
         return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
     }
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Integer getCustomer_id() {
-        return customer_id;
-    }
-
-    public void setCustomer_id(Integer customer_id) {
-        this.customer_id = customer_id;
-    }
-
     public List<Car> getCarList() {
         return carList;
     }
 
-    public void setCarList(List<Car> carList) {
-        this.carList = carList;
+    public Set<Role> getRoleSet() {
+        return roleSet;
     }
+
+
 }

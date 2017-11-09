7498abeb91acab6edbb54dce761678677ab84780
Tommy093 <tomek.wloszkiewicz@gmail.com>
2017-11-09 18:52:16 +0100

    +SecConfig+Customer+CusDetSeI+home

---
+++ src/main/java/com/mechanicproject/error/DefaultAuthenticationEntryPoint.java
@@ -0,0 +1,25 @@
+package com.mechanicproject.error;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by RENT on 2017-11-08.
+ */
+
+@Component
+public class DefaultAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e)
+            throws IOException, ServletException {
+        RequestDispatcher rd = httpServletRequest.getRequestDispatcher("/login?denied");
+        rd.forward(httpServletRequest, httpServletResponse);
+    }
+}

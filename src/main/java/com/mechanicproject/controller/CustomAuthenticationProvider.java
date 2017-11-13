569a82860bab8aa1bad50f742a9a192eaa0c5895
Tommy093 <tomek.wloszkiewicz@gmail.com>
2017-11-13 18:07:28 +0100

    +Registration

---
+++ src/main/java/com/mechanicproject/controller/CustomAuthenticationProvider.java
@@ -0,0 +1,55 @@
+package com.mechanicproject.controller;
+
+import com.mechanicproject.security.Role;
+import com.mechanicproject.service.CustomerService;
+import org.apache.log4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+	CustomerService customerService;
+
+	@Autowired
+	public CustomAuthenticationProvider(CustomerService customerService) {
+		this.customerService = customerService;
+	}
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		String name = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        
+        System.out.print("Name = " + name + " ,Password = " + password);
+        
+
+		System.out.print("Succesful authentication!");
+		customerService.getById(1);
+		Set<Role> roleSet = new HashSet<Role>();
+		Role role = Role.ROLE_USER;
+		roleSet.add(role);
+		List<SimpleGrantedAuthority> roles = roleSet.stream().map(p -> new SimpleGrantedAuthority(p.name())).collect(Collectors.toList());
+
+		return new UsernamePasswordAuthenticationToken(name, password, roles);
+
+	}
+ 
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
+	}
+ 
+}
